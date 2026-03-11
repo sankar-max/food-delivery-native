946a9de6d32117d4dca97ca8bf1bd6a0929aae52
sankar <sankrvj18@gmail.com>
2026-03-12 00:34:29 +0530

    feat: Initialize food delivery application with core screens, assets, and Tailwind CSS integration.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+import { useFonts } from "expo-font"
+import { SplashScreen, Stack } from "expo-router"
+import { useEffect } from "react"
+import "./global.css"
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  })
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+  useEffect(() => {
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync()
+  }, [fontsLoaded, error])
+  return <Stack screenOptions={{ headerShown: false }} />
 }
---
+++ components/CartBtn.tsx
@@ -0,0 +1,19 @@
+import { images } from "@/constants"
+import React from "react"
+import { Image, Text, TouchableOpacity, View } from "react-native"
+
+const CartBtn = () => {
+  const totalItems = 55
+  return (
+    <TouchableOpacity className="cart-btn">
+      <Image source={images.bag} resizeMode="contain" className="size-5" />
+      {totalItems > 0 && (
+        <View className={"cart-badge"}>
+          <Text className="small-bold text-white">{totalItems}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
+  )
+}
+
+export default CartBtn
